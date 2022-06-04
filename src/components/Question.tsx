608d08056f148d4eabd9ab651c0c0337e309aea6
douglasor <douglasor@outlook.com>
2022-06-03 21:13:57 -0300

    aula -5

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -7,14 +7,16 @@ type QuestionProps = {
         name: string;
         avatar: string;
     },
-    children?:ReactNode
+    children?: ReactNode,
+    isAnswered: boolean;
+    isHighLighted: boolean;
 }
 
 
-export function Question({ content, author,children }: QuestionProps) {
+export function Question({ content, author, children, isAnswered, isHighLighted }: QuestionProps) {
 
     return (
-        <div className="question">
+        <div className={`question ${isAnswered ? 'answered' : ''} ${isHighLighted && !isAnswered ? 'highlighted' : ''}`}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
